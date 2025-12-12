7437469048ae957abb62b71e5a2619f73c7e4d73
Callie Peden <calliepeden@gmail.com>
2025-12-12 14:30:26 -0500

    switch jovian conf

--- flake.nix
+++ flake.nix
@@ -1,13 +1,17 @@
 {
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		jovian = {
+			url = "github:Jovian-Experiments/Jovian-NixOS";
+		};
 	};
 
-	outputs = { self, nixpkgs }: {
+	outputs = { self, nixpkgs, jovian }: {
 		nixosConfigurations.deck = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			modules = [
 				./configuration.nix
+				jovian.nixosModules.jovian
 			];
 		};
 	};
