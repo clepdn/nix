9de0a1e1f334cd55d159fa343cd723294436cc4c
Callie Peden <calliepeden@gmail.com>
2026-01-16 03:13:12 -0500

    fix minio

--- hosts/homura-v/default.nix
+++ hosts/homura-v/default.nix
@@ -155,6 +155,7 @@
 
   systemd.tmpfiles.rules = [
 	"d /mnt/hdd/s3 0750 minio minio -"
+	"d /var/lib/tscl-minio/ 0700 tscl-minio tscl-minio -"
   ];
   
   age.secrets.tail = {
@@ -165,10 +166,11 @@
   };
 
   users.users.tscl-minio = {
-	isSystemUser = true;
+	isNormalUser = true;
+	createHome = true;
+	shell = pkgs.shadow; # /usr/bin/nologin for some reason
 	group = "tscl-minio";
 	description = "Tailscale user networking service user";
-	createHome = false;
   };
   users.groups.tscl-minio = {};
   
@@ -179,19 +181,12 @@
 	wantedBy = [ "multi-user.target" ];
 	serviceConfig = {
 		Type = "simple";
-		ExecStart = "${pkgs.tailscale}/bin/tailscaled --tun=userspace-networking --socket /run/tscl-minio/tailscaled-minio.sock";
-		ExecStartPost = "${pkgs.tailscale}/bin/tailscale --socket /run/tscl-minio/tailscaled-minio.sock up --login-server=https://vpn.gaze.systems --auth-key=file:${config.age.secrets.tail.path}";
+		ExecStart = "${pkgs.tailscale}/bin/tailscaled --tun=userspace-networking --socket /home/tscl-minio/tailscaled-minio.sock";
+		ExecStartPost = "${pkgs.tailscale}/bin/tailscale --socket /home/tscl-minio/tailscaled-minio.sock up --login-server=https://vpn.gaze.systems --auth-key=file:${config.age.secrets.tail.path}";
 		Restart = "on-failure";
 		RestartSec = "5s";
 		User = "tscl-minio";
 		Group = "tscl-minio";
-
-		RuntimeDirectory = "tscl-minio";
-		RuntimeDirectoryMode = "0755";
-		StateDirectory = "tscl-minio";
-		StateDirectoryMode = "0755";
-		CacheDirectory = "tscl-minio";
-		CacheDirectoryMode = "0755";
 	};
   };
 
