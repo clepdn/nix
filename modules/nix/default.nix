8c7f95d9f980fb1d556077b8db2e3f230469043e
Callie Peden <calliepeden@gmail.com>
2026-02-20 11:34:46 -0500

    NixOS (Yarara) generation 86

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -1,9 +1,22 @@
 { ... }:
 {
   nix.settings = {
-	experimental-features = [ "nix-command" "flakes" ];
-	trusted-users = [ "root" ];
+    experimental-features = [ "nix-command" "flakes" ];
+    trusted-users = [ "root" ];
   };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly"; # or "daily", "monthly", a systemd calendar string like "Mon *-*-* 03:00:00", etc.
+    options = "--delete-older-than 14d";
+  };
+
+  nix.optimise = {
+    automatic = true;
+    dates = [ "weekly" ];
+  };
+
+  nix.settings.auto-optimise-store = true;
 
   nixpkgs.config.allowUnfree = true;
 }
