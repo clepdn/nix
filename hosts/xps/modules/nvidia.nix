dc34e665c382ae013fd8af3771699b1324d5cde8
Callie Peden <calliepeden@gmail.com>
2026-02-23 21:03:33 -0500

    NixOS (Yarara) generation 88

--- hosts/xps/modules/nvidia.nix
+++ hosts/xps/modules/nvidia.nix
@@ -10,11 +10,21 @@
 		options bbswitch load_state=0 unload_state=1	
 	'';
 
-
 	boot.extraModulePackages = [ 
 		config.boot.kernelPackages.bbswitch
-		config.boot.kernelPackages.nvidiaPackages.stable
+		# config.boot.kernelPackages.nvidiaPackages.stable
 	];
 
-	hardware.nvidia.open = true;
+	services.xserver.videoDrivers = [ "nvidia" ];
+
+	hardware.nvidia = {
+		modesetting.enable = true;
+		powerManagement.enable = false;
+		open = true;
+		prime = {
+			offload.enable = true;
+			intelBusId = "PCI:0:2:0";
+			nvidiaBusId = "PCI:1:0:0";
+		};
+	};
 }
