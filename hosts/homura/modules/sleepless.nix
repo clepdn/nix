11c02a8a6f9e1c8040591228ca812ce83ce7e351
Callie Peden <calliepeden@gmail.com>
2026-03-20 13:30:31 -0400

    no sleeping

---
+++ hosts/homura/modules/sleepless.nix
@@ -0,0 +1,18 @@
+{
+  services.logind = {
+    lidSwitch = "ignore";          # ignore lid close
+    extraConfig = ''
+      HandleSuspendKey=ignore
+      HandleHibernateKey=ignore
+      HandleLidSwitch=ignore
+      HandleLidSwitchExternalPower=ignore
+    '';
+  };
+
+  systemd.targets = {
+    sleep.enable = false;
+    suspend.enable = false;
+    hibernate.enable = false;
+    "hybrid-sleep".enable = false;
+  };
+}
