fc5258e9005bf2df841024975de93c888260082a
Callie Peden <calliepeden@gmail.com>
2026-03-29 00:13:06 -0400

    NixOS (Yarara) generation 106

--- hosts/homura/modules/wyoming-faster-whisper.nix
+++ hosts/homura/modules/wyoming-faster-whisper.nix
@@ -1,26 +1,10 @@
 { ... }:
 {
-  hardware.nvidia-container-toolkit.enable = true;
-
-  virtualisation.oci-containers.containers.wyoming-faster-whisper = {
-    image = "rhasspy/wyoming-whisper:latest";
-    ports = [ "10300:10300" ];
-    volumes = [
-      "/var/lib/wyoming-faster-whisper:/data:rw"
-    ];
-    cmd = [
-      "--model" "small"
-      "--language" "en"
-      "--device" "cuda"
-      "--compute-type" "int8"
-    ];
-    extraOptions = [
-      "--device=nvidia.com/gpu=all"
-    ];
-    log-driver = "journald";
+  services.wyoming.faster-whisper.servers.main = {
+    enable = true;
+    model = "small-int8";
+    language = "en";
+    device = "cuda";
+    uri = "tcp://0.0.0.0:10300";
   };
-
-  systemd.tmpfiles.rules = [
-    "d /var/lib/wyoming-faster-whisper 0755 root root -"
-  ];
 }
