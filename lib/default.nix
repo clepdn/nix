df6a043b5a0ba4ae160a266e365ba7026be6d5fb
Callie Peden <calliepeden@gmail.com>
2026-04-10 18:26:34 -0400

    move files

---
+++ lib/default.nix
@@ -0,0 +1,13 @@
+lib:
+lib.makeExtensible (self: {
+  importFolder =
+    dir:
+    let
+      files = builtins.readDir dir;
+      fileNames = builtins.attrNames files;
+      filesToImport = builtins.map (name: "${dir}/${name}") (
+        builtins.filter (name: (builtins.match ".*.nix" name != null) && (name != "default.nix")) fileNames
+      );
+    in
+    filesToImport;
+})
---
+++ pkgs/helium.nix
@@ -0,0 +1,95 @@
+{ lib
+, stdenv
+, fetchurl
+, autoPatchelfHook
+, alsa-lib
+, mesa
+, nss
+, nspr
+, xorg
+, libdrm
+, libxkbcommon
+, expat
+, cups
+, dbus
+, at-spi2-atk
+, pango
+, cairo
+, glib
+, gtk3
+}:
+stdenv.mkDerivation rec {
+  pname = "helium";
+  version = "0.10.9.1";
+
+  src =
+    let
+      platformMap = {
+        "x86_64-linux" = "x86_64_linux";
+        "aarch64-linux" = "arm64_linux";
+      };
+
+      platform = platformMap.${stdenv.hostPlatform.system};
+
+      hashes = {
+        "x86_64-linux" = lib.fakeHash;
+        "aarch64-linux" = lib.fakeHash;
+      };
+
+      hash = hashes.${stdenv.hostPlatform.system};
+    in
+    fetchurl {
+      url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-${platform}.tar.xz";
+      inherit hash;
+    };
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+  buildInputs = [
+    alsa-lib
+    mesa
+    nss
+    nspr
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libxcb
+    libdrm
+    libxkbcommon
+    expat
+    cups
+    dbus
+    at-spi2-atk
+    pango
+    cairo
+    glib
+    gtk3
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/bin"
+    mkdir -p "$out/share/lib/helium"
+
+    cp -r . "$out/share/lib/helium/"
+    ln -s "$out/share/lib/helium/helium" "$out/bin/helium"
+
+    cp -r opt/helium/locales "$out/share/lib/helium/"
+    cp -r usr/share "$out/" 2>/dev/null || true
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Private, fast, and honest web browser based on Chromium";
+    homepage = "https://github.com/imputnet/helium-chromium";
+    changelog = "https://github.com/imputnet/helium-linux/releases/tag/${version}";
+    platforms = [ "x86_64-linux" "aarch64-linux" ];
+    license = lib.licenses.gpl3;
+    mainProgram = "helium";
+  };
+}
