67098904c30d62aa7e0b773e990303fbc0c03847
Callie Peden <calliepeden@gmail.com>
2026-05-11 16:13:03 -0400

    vision gemma e4b

--- modules/llama-cpp/default.nix
+++ modules/llama-cpp/default.nix
@@ -4,9 +4,9 @@ let
   pkg = (pkgs.llama-cpp.override { cudaSupport = true; }).overrideAttrs (old: {
     src = inputs.llama-cpp-src;
     version = "0";
-    npmDepsHash = "sha256-eeftjKt0FuS0Dybez+Iz9VTVMA4/oQVh+3VoIqvhVMw=";
+    npmDepsHash = "sha256-iYJB0z2YHG8OzEA9EwHUZrDa5obr5m2sbnIH+of28o0=";
     postPatch = "rm -f tools/server/public/index.html.gz";
-    cmakeFlags = (old.cmakeFlags or []) ++ ["-DCMAKE_CUDA_ARCHITECTURES=61"];
+    cmakeFlags = (old.cmakeFlags or []) ++ ["-DCMAKE_CUDA_ARCHITECTURES=61" "-DGGML_CUDA_FA=OFF"];
   });
 in {
   age.secrets.llama-api-key = {
@@ -27,6 +27,7 @@ in {
         --host 0.0.0.0 \
         --port ${toString servicePort} \
         -m /var/lib/llama/gemma4-e4b.gguf \
+        --mmproj "/var/lib/llama/mmproj-Gemma-4-E4B-Uncensored-HauhauCS-Aggressive-f16.gguf"
         --api-key "$(< ${config.age.secrets.llama-api-key.path})"
     ''
   );
