db200851554852f70112957e3e618fa2f64b9cc3
Callie Peden <calliepeden@gmail.com>
2026-06-03 20:27:20 -0400

    coral webhook

---
+++ hosts/homura/modules/coral-webhook.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, self, ... }:
+let
+  rebuildScript = pkgs.writeShellApplication {
+    name = "coral-rebuild";
+    runtimeInputs = [ pkgs.nixos-container ];
+    text = builtins.readFile ./coral-webhook-handler.sh;
+  };
+in
+{
+  age.secrets.coralWebhookToken = {
+    file = "${self}/secrets/coral-webhook-token.age";
+    mode = "0400";
+    owner = "root";
+  };
+
+  systemd.sockets.coral-webhook = {
+    description = "Coral rebuild webhook socket";
+    wantedBy = [ "sockets.target" ];
+    listenStreams = [ "127.0.0.1:9123" ];
+    socketConfig = {
+      Accept = true;
+      MaxConnections = 5;
+    };
+  };
+
+  systemd.services."coral-webhook@" = {
+    description = "Coral rebuild webhook handler";
+    environment = {
+      WEBHOOK_TOKEN_FILE = config.age.secrets.coralWebhookToken.path;
+    };
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${rebuildScript}/bin/coral-rebuild";
+      StandardInput = "socket";
+      StandardOutput = "socket";
+      StandardError = "journal";
+      User = "root";
+      Group = "root";
+      SupplementaryGroups = [ config.users.groups.keys.name ];
+    };
+  };
+
+  networking.firewall.interfaces.lo.allowedTCPPorts = [ 9123 ];
+}
