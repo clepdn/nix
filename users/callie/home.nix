15ecdb70109313e98ffc601ac2f08a5f87c7c047
Callie Peden <calliepeden@gmail.com>
2026-06-25 13:17:13 -0400

    no more pi config mgmt
    home, it's where i want to be pick me up and turn me round

--- users/callie/home.nix
+++ users/callie/home.nix
@@ -28,24 +28,4 @@
     ];
     stateVersion = "25.11";
   };
-
-  home.activation.piSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    settings="$HOME/.pi/agent/settings.json"
-    mkdir -p "$(dirname "$settings")"
-    if [ ! -f "$settings" ]; then
-      echo '{}' > "$settings"
-    fi
-    current=$(cat "$settings")
-    echo "$current" \
-      | ${pkgs.jq}/bin/jq '
-          .defaultProvider = "anthropic"
-          | .defaultThinkingLevel = "medium"
-          | .packages = [
-              "https://github.com/nicobailon/pi-web-access",
-              "https://github.com/nicobailon/pi-subagents"
-            ]
-        ' > "$settings.tmp" && mv "$settings.tmp" "$settings"
-  '';
-
-
 }
