2387bf51ecac6bb81bd3b7d00b9647e250fcfadc
Callie Peden <calliepeden@gmail.com>
2026-07-19 21:27:06 -0400

    nautilus

--- users/callie/graphicalSession.nix
+++ users/callie/graphicalSession.nix
@@ -4,21 +4,32 @@
     ./default.nix
   ];
 
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
+
   users.users.callie.packages = with pkgs; [
     spotify
     feishin
+
     thunderbird
     qdirstat
     kdePackages.kate
+
     ente-auth
     signal-desktop
+
     mpv
+
     pwvucontrol
     pavucontrol
+
     crosspipe
     livecaptions
     helvum
     mypkgs.helium
+
+    nautilus
+    sshfs
 
     (discord.override {
       withMoonlight = true;
