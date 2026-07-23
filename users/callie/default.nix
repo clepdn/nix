36a74abc73135fcb5cea2ec5e995f559155f68b8
Callie Peden <calliepeden@gmail.com>
2026-07-23 01:15:08 -0400

    ensure plugdev exists

--- users/callie/default.nix
+++ users/callie/default.nix
@@ -1,8 +1,10 @@
 { ... }:
 {
   imports = [ ./account.nix ];
-
+  
+  # For the switch.
   users.users.callie.extraGroups = [ "plugdev" ];
+  users.groups.plugdev = {};
 
   home-manager.users.callie = import ./home.nix;
 }
