1a40b31d2b8610da25614ca02bb502e90d6fa9d4
Callie Peden <calliepeden@gmail.com>
2026-08-13 20:15:19 -0400

    awww wallpaper

--- hosts/madoka/modules/niri.nix
+++ hosts/madoka/modules/niri.nix
@@ -1,4 +1,4 @@
-{ self, ... }:
+{ self, pkgs, ... }:
 {
   imports = [
     "${self}/modules/niri"
@@ -12,5 +12,16 @@
   home-manager.users.callie.programs.niri.settings.outputs."eDP-1" = {
     mode = { width = 1920; height = 1200; refresh = 59.950; };
     scale = 1.25;
+  };
+
+  systemd.user.services.awww-wallpaper = {
+    description = "Hatsune Miku wallpaper";
+    wantedBy = [ "niri.service" ];
+    after = [ "awww-daemon.service" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.awww}/bin/awww img ${self}/assets/hatsunemiku.jpg";
+    };
   };
 }
